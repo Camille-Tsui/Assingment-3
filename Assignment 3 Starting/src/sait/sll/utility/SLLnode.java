06f2c1c74510875e9ba1a424ff2c38980d4e7562
Camille Tsui <camille.tsui@edu.sait.ca>
2026-04-03 14:11:16 -0600

    create node class

--- Assignment 3 Starting/src/sait/sll/utility/SLLnode.java
+++ Assignment 3 Starting/src/sait/sll/utility/SLLnode.java
@@ -2,11 +2,43 @@ package sait.sll.utility;
 
 import java.io.Serializable;
 
+import sait.sll.problemdomain.User;
+
 public class SLLnode implements Serializable{
+	
+	private User user;
+	private SLLnode next;
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -4557626414187024239L;
+	
+	public SLLnode(User user) {
+		this.user = user;
+	}
+	
+	public SLLnode(User user, SLLnode next) {
+		this.user = user;
+		this.next = next;
+	}
+
+	
+	public User getUser() {
+		return user;
+	}
+
+	public SLLnode getNext() {
+		return next;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public void setNext(SLLnode next) {
+		this.next = next;
+	}
+	
 
 }
