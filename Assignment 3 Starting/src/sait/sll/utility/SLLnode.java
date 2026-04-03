13c9e7187a060720241ccc414f55fa9a034d63e9
Camille Tsui <camille.tsui@edu.sait.ca>
2026-04-03 13:49:36 -0600

    innitial commit

---
+++ Assignment 3 Starting/src/sait/sll/utility/SLLnode.java
@@ -0,0 +1,12 @@
+package sait.sll.utility;
+
+import java.io.Serializable;
+
+public class SLLnode implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4557626414187024239L;
+
+}
