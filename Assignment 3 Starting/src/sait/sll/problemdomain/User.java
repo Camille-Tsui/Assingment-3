755bee72c1bd99f40be9a24c07fcfb863d0d8bb3
earlysakura-hub <earlysakura@gmail.com>
2026-04-09 10:28:40 -0600

    Update User.java

--- Assignment 3 Starting/src/sait/sll/problemdomain/User.java
+++ Assignment 3 Starting/src/sait/sll/problemdomain/User.java
@@ -20,7 +20,7 @@ public class User implements Serializable{
 	
 	private String email;
 	
-	private String password;
+	private transient String password;
 	
 	/**
 	 * Creates a User object with specified arguments.
